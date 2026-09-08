fd958920cf6f63bcac90ab74d3ea382f5daad6a4
Bruno Tarditi <brunotarditi@hotmail.com>
2026-09-08 20:05:35 -0300

    feat: integracion con peak auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   api: 'https://apimariela.brunotarditi.com/api/v1/',
+  peakAuthUrl: 'URL_PEAK_AUTH',
+  peakAuthClientId: 'TU_CLIENT_ID_AQUI',
 };
