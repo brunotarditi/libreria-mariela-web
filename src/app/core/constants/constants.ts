9aa246a049303e4c99d12273cd6ef716af9e4f21
Bruno Tarditi <brunotarditi@hotmail.com>
2026-09-09 20:18:01 -0300

    style: formatea constantes en constants.ts

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -1,17 +1,17 @@
 export const ACCESS_TOKEN = 'Access';
 
 export const ROLES = {
-  ROOT: 'ROOT',
-  ADMIN: 'ADMIN',
-  WRITE: 'WRITE',
-  READ: 'READ',
+    ROOT: 'ROOT',
+    ADMIN: 'ADMIN',
+    WRITE: 'WRITE',
+    READ: 'READ',
 } as const;
 
 export type Role = (typeof ROLES)[keyof typeof ROLES];
 
 export const ALLOWED_ROLES: string[] = [
-  ROLES.ROOT,
-  ROLES.ADMIN,
-  ROLES.WRITE,
-  ROLES.READ,
+    ROLES.ROOT,
+    ROLES.ADMIN,
+    ROLES.WRITE,
+    ROLES.READ,
 ];
